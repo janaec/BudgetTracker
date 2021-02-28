e8ead5cf2f20a0c3d84f14c3b82599ce2ea75dde
email <jjazminclrk@gmail.com>
2021-02-27 20:03:57 -0500

    added remaining code to db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 let db = request.result;
 const request = window.indexDB.open("budget", 1);
-request.onupgradeneeded = function(event) {
-    const budgetStore = db.createObjectStore ("pending");
+request.onupgradeneeded = function (event) {
+    const budgetStore = db.createObjectStore("pending");
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     const db = target.result;
 
     if (navigator.onLine) {
@@ -12,6 +12,32 @@ request.onsuccess = function(event) {
     }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
 
 };
+function saveRecord(record) {
+    const trasaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = trasanction.objectStore("pending");
+}
+
+function checkDatabase() {
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+
+                });
+        }
+
+
+    };
+    }
+    window.addEventListener("online", checkDatabase);
